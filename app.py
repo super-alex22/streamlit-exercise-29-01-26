06c79b9fbcc7130917a8170d097dcf1502750ce0
super-alex22 <116815031+super-alex22@users.noreply.github.com>
2026-01-29 10:41:53 +0200

    Implement number input with conditional responses
    Add number input and conditional messages based on value

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ if answer == "yes":
   st.write("Spectacular! Keep it up")
 else:
   st.write("You dont seem to have a choice")
+st.write("I'd like to humbly ask your Mightiness to enter number")
+number = st.number_input("Have the number entered in this field")
+if(number>10):
+  st.write("The number is enormous")
+else:
+  st.write("The number is miserable")
