ee54f70a3d96e4d2317cb2d205eb8c2225d08bde
super-alex22 <116815031+super-alex22@users.noreply.github.com>
2026-01-29 10:38:50 +0200

    Add Streamlit interaction for Python preference

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-
+import streamlit as st
+st.write("Do you like python?")
+answer = st.radio("Do you love python", ("yes", "no"))
+if answer == "yes":
+  st.write("Spectacular! Keep it up")
+else:
+  st.write("You dont seem to have a choice")
